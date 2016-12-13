db5f79d1430c265e7bed11b8815e9529daf7cabd
mxm2005 <mxmmm18@163.com>
2016-12-13 15:45:58 +0800

    mod nav bar

--- deker_mvc/Controllers/HomeController.cs
+++ deker_mvc/Controllers/HomeController.cs
@@ -14,6 +14,7 @@ namespace deker_mvc.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.hideNav = "true";
             ViewBag.RecommendProd = RecommendProduct.Instance.GetRecommendProd();
             return View();
         }
